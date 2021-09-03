c22e953eee4cc6d1bdf5cae825b3b7d92460596d
manujo-varghese <exit()>
2021-09-03 19:28:42 +1000

    initial commit

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+const amount =2
+
+if (amount< 12)
+{
+    console.log('small number');
+}
+else
+{
+    console.log('large number');  
+}
+
+console.log('hey its my first node app!!')
---
+++ module.js
@@ -0,0 +1,9 @@
+const names = require('./4-names');
+const functionNames = require('./5-utils');
+const data = require('./6-alernative-flavor');
+require('./7-mind-grenade')
+//console.log(names)
+// functionNames.sayHi('susan')
+// functionNames.sayHi(names.john)
+// functionNames.sayHi(names.peter)
+//console.log(data.singlePerson.name)
